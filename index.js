ca7172c90bbed1cec90566669468c6c02537b256
Andrew Matthews <andyrew3000@gmail.com>
2024-12-01 12:37:31 -0500

    Database on branch a6

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-import express from 'express';
+import express from "express";
+import mongoose from "mongoose";
 import Hello from "./Hello.js";
 import Lab5 from "./Lab5/index.js";
 import cors from "cors";
@@ -9,6 +10,9 @@ import AssignmentRoutes from './Kanbas/Assignments/routes.js';
 import EnrollmentsRoutes from './Kanbas/Enrollments/routes.js';
 import session from "express-session";
 import "dotenv/config";
+
+const CONNECTION_STRING = process.env.MONGO_CONNECTION_STRING || "mongodb://127.0.0.1:27017/kanbas" 
+mongoose.connect(CONNECTION_STRING);
 const app = express();
 app.use(cors({
     credentials: true,
